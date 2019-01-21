1a34df82d6adc6ea6623b4544cd00130796e3726
spotapov <spotapov.spt@gmail.com>
2019-01-21 21:03:41 +0300

    image_converter: поддержка roi

--- dh-software/libraries/image_processing/source/image_converter.h
+++ dh-software/libraries/image_processing/source/image_converter.h
@@ -5,10 +5,12 @@
 
 #include <opencv2/core.hpp>
 
+#include <ippcore.h>
+
 class image_converter
 {
 public:
-	static void convert_8u_32f( const cv::Mat& image_8u, cv::Mat& image_32f );
-	static void convert_32f_8u( const cv::Mat& image_32f, cv::Mat& image_8u );
+    static void convert_8u_32f( const cv::Mat& image_8u, cv::Mat& image_32f, IppiSize roi = { -1, -1 } );
+    static void convert_32f_8u( const cv::Mat& image_32f, cv::Mat& image_8u, IppiSize roi = { -1, -1 });
 };
 
--- dh-software/libraries/image_processing/tests/image_converter_tests.cpp
+++ dh-software/libraries/image_processing/tests/image_converter_tests.cpp
@@ -1,6 +1,6 @@
 #include "image_converter.h"
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 using namespace cv;
 using namespace testing;
@@ -8,25 +8,28 @@ using namespace testing;
 TEST( image_converter_tests, convert_8u_32f_different_size_throws_exception )
 {
 	{
-		auto image_8u = Mat( 3, 3, CV_8UC1 );
-		auto image_32f = Mat( 2, 3, CV_32FC1 );
+        auto image_8u = Mat( 3, 3, CV_8UC1 );
+        auto image_32f = Mat( 4, 4, CV_32FC1 );
 
-		EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f ), argument_exception );
+        EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f, { 4, 3 } ), argument_exception );
+        EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f, { 3, 4 } ), argument_exception );
 	}
 
-	{
-		auto image_8u = Mat( 3, 3, CV_8UC1 );
-		auto image_32f = Mat( 3, 2, CV_32FC1 );
+    {
+        auto image_8u = Mat( 4, 4, CV_8UC1 );
+        auto image_32f = Mat( 3, 3, CV_32FC1 );
 
-		EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f ), argument_exception );
-	}
+        EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f, { 4, 3 } ), argument_exception );
+        EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f, { 3, 4 } ), argument_exception );
+    }
+}
 
-	{
-		auto image_8u = Mat( 3, 3, CV_8UC1 );
-		auto image_32f = Mat( 3, 3, CV_32FC3 );
+TEST( image_converter_tests, convert_8u_32f_different_channels_throws_exception )
+{
+    auto image_8u = Mat( 3, 3, CV_8UC1 );
+    auto image_32f = Mat( 3, 3, CV_32FC3 );
 
-		EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f ), argument_exception );
-	}
+    EXPECT_THROW( image_converter::convert_8u_32f( image_8u, image_32f ), argument_exception );
 }
 
 TEST( image_converter_tests, convert_8u_32f_wrong_format_throws_exception )
@@ -107,29 +110,31 @@ TEST( image_converter_tests, convert_8u_32f_c3_works )
 		}
 }
 
-
 TEST( image_converter_tests, convert_32f_8u_different_size_throws_exception )
 {
-	{
-		auto image_32f = Mat( 3, 3, CV_32FC1 );
-		auto image_8u = Mat( 2, 3, CV_8UC1 );
+    {
+        auto image_32f = Mat( 3, 3, CV_32FC1 );
+        auto image_8u = Mat( 4, 4, CV_8UC1 );
 
-		EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u ), argument_exception );
-	}
+        EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u, { 4, 3 } ), argument_exception );
+        EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u, { 3, 4 } ), argument_exception );
+    }
 
-	{
-		auto image_32f = Mat( 3, 3, CV_32FC1 );
-		auto image_8u = Mat( 3, 2, CV_8UC1 );
+    {
+        auto image_32f = Mat( 4, 4, CV_32FC1 );
+        auto image_8u = Mat( 3, 3, CV_8UC1 );
 
-		EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u ), argument_exception );
-	}
+        EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u, { 4, 3 } ), argument_exception );
+        EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u, { 3, 4 } ), argument_exception );
+    }
+}
 
-	{
-		auto image_32f = Mat( 3, 3, CV_32FC1 );
-		auto image_8u = Mat( 2, 3, CV_8UC3 );
+TEST( image_converter_tests, convert_32f_8u_different_channels_throws_exception )
+{
+    auto image_32f = Mat( 3, 3, CV_32FC3 );
+    auto image_8u = Mat( 3, 3, CV_8UC1 );
 
-		EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u ), argument_exception );
-	}
+    EXPECT_THROW( image_converter::convert_32f_8u( image_32f, image_8u ), argument_exception );
 }
 
 TEST( image_converter_tests, convert_32f_8u_wrong_format_throws_exception )
