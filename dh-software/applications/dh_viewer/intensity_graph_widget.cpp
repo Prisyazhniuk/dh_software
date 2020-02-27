21e17b215c47a712f0310a75bb788123e6f1435a
spotapov <spotapov.spt@gmail.com>
2020-02-27 21:09:57 +0300

    #13 Нормировка осей графика интенсивности

--- dh-software/applications/dh_viewer/intensity_graph_widget.cpp
+++ dh-software/applications/dh_viewer/intensity_graph_widget.cpp
@@ -1,6 +1,9 @@
 #include "intensity_graph_widget.h"
 
 #include <QLineSeries>
+#include <QValueAxis>
+
+#include <math.h>
 
 namespace dh
 {
@@ -16,23 +19,58 @@ namespace dh
 
     void intensity_graph_widget::plot( const QImage& image, const QList<QPoint>& points )
     {
-        _chart->removeAllSeries();
-
-        auto series = new QLineSeries();
-
-        int n = 0;
         auto rect = image.rect();
+
+        int min = 255;
+        int max = 0;
+
         for( auto p: points )
         {
             if( !rect.contains( p ) )
                 continue;
 
             auto px = qGray( image.pixel( p ) );
-            series->append( n++, px );
+
+            if( px > max )
+                max = px;
+
+            if( px < min )
+                min = px;
+        }
+
+
+        _chart->removeAllSeries();
+        auto series = new QLineSeries();
+
+        QPoint start_point = *points.begin();
+        double image_width = image.width();
+        double range = ( max - min ) != 0 ? max - min : 1;
+
+        for( auto p: points )
+        {
+            if( !rect.contains( p ) )
+                continue;
+
+            qreal px = qGray( image.pixel( p ) );
+            px = ( px - min ) / range;
+
+            auto dx = start_point.x() - p.x();
+            auto dy = start_point.y() - p.y();
+            auto distance = sqrt( dx*dx + dy*dy );
+            auto len = distance / image_width;
+
+            series->append( len, px );
         }
 
         _chart->addSeries( series );
         _chart->createDefaultAxes();
+
+        auto axes = _chart->axes( Qt::Horizontal );
+        if( !axes.empty())
+        {
+            auto a = static_cast<QValueAxis *>( *axes.begin() );
+            a->setTickCount( 5 );
+        }
     }
 
     void intensity_graph_widget::clean()
